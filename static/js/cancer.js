cd50a4bf5767e68638c14f199ef849398732395f
Jose <o.castillojl@gmail.com>
2017-07-25 00:24:24 -0500

    front-end finished

---
+++ static/js/cancer.js
@@ -0,0 +1,51 @@
+var slider = new Slider("#Clump_Thickness");
+slider.on("slide", function(sliderValue) {
+  document.getElementById("ex6SliderVal").textContent = sliderValue;
+});
+var slider1 = new Slider("#Uniformity_of_Cell_Size");
+slider1.on("slide", function(sliderValue) {
+  document.getElementById("ex6SliderVal1").textContent = sliderValue;
+});
+var slider2 = new Slider("#Uniformity_of_Cell_Shape");
+slider2.on("slide", function(sliderValue) {
+  document.getElementById("ex6SliderVal2").textContent = sliderValue;
+});
+var slider3 = new Slider("#Marginal_Adhesion");
+slider3.on("slide", function(sliderValue) {
+  document.getElementById("ex6SliderVal3").textContent = sliderValue;
+});
+var slider4 = new Slider("#Single_Epithelial_Cell_Size");
+slider4.on("slide", function(sliderValue) {
+  document.getElementById("ex6SliderVal4").textContent = sliderValue;
+});
+var slider5 = new Slider("#Bare_Nuclei");
+slider5.on("slide", function(sliderValue) {
+  document.getElementById("ex6SliderVal5").textContent = sliderValue;
+});
+var slider6 = new Slider("#Bland_Chromatin");
+slider6.on("slide", function(sliderValue) {
+  document.getElementById("ex6SliderVal6").textContent = sliderValue;
+});
+var slider7 = new Slider("#Normal_Nucleoli");
+slider7.on("slide", function(sliderValue) {
+  document.getElementById("ex6SliderVal7").textContent = sliderValue;
+});
+var slider8 = new Slider("#Mitoses");
+slider8.on("slide", function(sliderValue) {
+  document.getElementById("ex6SliderVal8").textContent = sliderValue;
+});
+
+
+function getValues() {
+  var table, tr;
+  var td = [];
+  table = document.getElementById('attributes');
+  tr = table.getElementsByTagName('tr');
+  for (var i = 0; i < tr.length; i++) {
+    alert(document.getElementById("attributes").rows[i].cells[2].innerHTML);
+  }
+  return td;
+}
+
+
+var text = "The temperature is  Celsius";
